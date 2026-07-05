cbea70f75010e6492ab41f60f4649fad150ace01
Shubham Sharma <shubham02242006@gmail.com>
2026-07-05 11:21:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/shubham/basiccrudapi/BasicCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.basiccrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/basiccrudapi/BasicCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.basiccrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
